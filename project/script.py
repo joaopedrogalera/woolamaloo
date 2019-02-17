49bfabda90d160d9b36aa2e55aab266dc22b289c
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-02-17 13:06:36 -0300

    Organizing data report in a list to tabulate. Adding comments

--- project/script.py
+++ project/script.py
@@ -6,24 +6,28 @@ import csv
 print("Woolamaloo Syncronization Report")
 print("--------------------------------")
 
-ldap_employees = {}
+ldap_employees = {} #Create the dictionary ldap_employees
 with open("ldap_export.csv",'r') as ldap_export:
-    ldap_row = csv.DictReader(ldap_export)
+    ldap_data = csv.DictReader(ldap_export) #Opens the .csv file with the ldap exported data and puts in a dictionary
 
-    for row in ldap_row:
-        ldap_employees[row["employeeNumber"]]=row
+    for row in ldap_data:
+        ldap_employees[row["employeeNumber"]]=row #Puts each line from csv file in the ldap_employees using the employeeNumber as key
 
-with open("employees_data.txt","r") as employees_data: #Open the file with the Employees data
-    employees_row = csv.DictReader(employees_data)
+tableData = [] #Create a list which will be used to make the table with the exports
+with open("employees_data.txt","r") as employees_export:
+    employees_data = csv.DictReader(employees_export) #Opens the file with the employees exported data and puts in a dictionary
 
-    for row in employees_row:
-        if len(row["demission_date"])!=0:
-            if row["id"] in ldap_employees:
-                print(row["name"] + " worked at Woolamaloo from " + row["admission_date"] + " to " + row["demission_date"])
-        else:
-            if not row["id"] in ldap_employees:
-                print(row["name"] + " Started to work at Woolamaloo on " + row["admission_date"])
-            elif row["department"] != ldap_employees[row["id"]]["department"]:
-                print(row["name"] + " changed department. Was " + ldap_employees[row["id"]]["department"] + " and now is " + row["department"])
-            elif row["position"] != ldap_employees[row["id"]]["position"]:
-                print(row["name"] + " changed position. Was " + ldap_employees[row["id"]]["position"] + " and now is " + row["position"])
+    for row in employees_data: #For each entry
+        if len(row["demission_date"])!=0: #If the employee doen't work at Woolamaloo anymore
+            if row["id"] in ldap_employees: #And still is in the LDAP
+                tableData.append([row["id"],row["name"],"Worked at Woolamaloo from " + row["admission_date"] + " to " + row["demission_date"]]) #Add an item on the tableData list with the report for this employee
+        else: #If the employee still works at Woolamaloo
+            if not row["id"] in ldap_employees: #and isn't in the LDAP
+                tableData.append([row["id"],row["name"],"Started to work at Woolamaloo on " + row["admission_date"]]) #Add an item on the tableData list with the report for this employee
+            elif row["department"] != ldap_employees[row["id"]]["department"]: #And department in the LDAP is distinct from the employees system
+                tableData.append([row["id"],row["name"],"Changed department. Was " + ldap_employees[row["id"]]["department"] + " and now is " + row["department"]]) #Add an item on the tableData list with the report for this employee
+            elif row["position"] != ldap_employees[row["id"]]["position"]: #And position in the LDAP is distinct from the employees system
+                tableData.append([row["id"],row["name"],"Changed position. Was " + ldap_employees[row["id"]]["position"] + " and now is " + row["position"]]) #Add an item on the tableData list with the report for this employee
+
+for i in tableData:
+    print(i[0] + "  " + i[1] + "  " + i[2])
